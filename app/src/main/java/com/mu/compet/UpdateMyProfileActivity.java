0329a036d31879a5482c2c056fe0411b7ea657ba
Park Museong <goodness897@gmail.com>
2016-10-23 23:50:22 +0900

    add camera gallery

--- app/src/main/java/com/mu/compet/UpdateMyProfileActivity.java
+++ app/src/main/java/com/mu/compet/UpdateMyProfileActivity.java
@@ -1,18 +1,42 @@
 package com.mu.compet;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
 
 public class UpdateMyProfileActivity extends AppCompatActivity {
 
     EditText nickNameEditText;
+    TextView changeImageText;
+
+    private String img_file_path;
+    File mSavedFile, mContentFile;
+    private static final int RC_GET_IMAGE = 1;
+    private static final int RC_CAMERA = 2;
+
+    ImageView profileView;
+
+    SelectImageCheckDialogFragment dialogFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +45,37 @@ public class UpdateMyProfileActivity extends AppCompatActivity {
         initToolBar("프로필 편집");
 
         nickNameEditText = (EditText) findViewById(R.id.edit_my_nickname);
+        changeImageText = (TextView) findViewById(R.id.text_change_image);
+        profileView = (ImageView) findViewById(R.id.image_my_profile);
+
+        if (savedInstanceState != null) {
+            String path = savedInstanceState.getString("savedfile");
+            if (!TextUtils.isEmpty(path)) {
+                mSavedFile = new File(path);
+            }
+            path = savedInstanceState.getString("contentfile");
+            if (!TextUtils.isEmpty(path)) {
+                mContentFile = new File(path);
+                int dstWidth = 200;
+                int dstHeight = 200;
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                Bitmap bmImg = BitmapFactory.decodeFile(mContentFile.getAbsolutePath(), options);
+                Bitmap resized = Bitmap.createScaledBitmap( bmImg, dstWidth, dstHeight, true );
+                profileView.setImageBitmap(resized);
+
+                profileView.setImageBitmap(bmImg);
+            }
+        }
+
+
+        changeImageText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogFragment = new SelectImageCheckDialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "select image");
+            }
+        });
 
         Button initButton = (Button) findViewById(R.id.btn_init);
         initButton.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +84,17 @@ public class UpdateMyProfileActivity extends AppCompatActivity {
                 nickNameEditText.setText("");
             }
         });
+    }
+
+    public Uri getSaveFile() {
+        File dir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES
+        ), "my_image");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        mSavedFile = new File(dir, "my_picture_" + System.currentTimeMillis() + ".jpg");
+        return Uri.fromFile(mSavedFile);
     }
 
     private void initToolBar(String title) {
@@ -65,4 +131,52 @@ public class UpdateMyProfileActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mSavedFile != null) {
+            outState.putString("savedfile", mSavedFile.getAbsolutePath());
+        }
+        if (mContentFile != null) {
+            outState.putString("contentfile", mContentFile.getAbsolutePath());
+        }
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+
+            case RC_CAMERA:
+                if (resultCode == Activity.RESULT_OK) {
+                    mContentFile = mSavedFile;
+                    Log.i("Image", "path : " + mContentFile.getAbsolutePath());
+
+                }
+                break;
+
+            case RC_GET_IMAGE:
+                if (resultCode == Activity.RESULT_OK) {
+                    dialogFragment.dismiss();
+                    Uri fileUri = intent.getData();
+                    Cursor c = getContentResolver().query(fileUri, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
+                    int dstWidth = 200;
+                    int dstHeight = 200;
+                    if (c.moveToNext()) {
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inSampleSize = 4;
+                        String path = c.getString(c.getColumnIndex(MediaStore.Images.Media.DATA));
+                        Bitmap bmImg = BitmapFactory.decodeFile(path, options);
+                        Bitmap resized = Bitmap.createScaledBitmap( bmImg, dstWidth, dstHeight, true );
+                        profileView.setImageBitmap(resized);
+                        Log.i("Image", "path : " + path);
+                    }
+                    break;
+                }
+        }
+    }
+
 }
+
